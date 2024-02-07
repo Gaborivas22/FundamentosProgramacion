1a690e5736a3a33679d9191e0e4d860b57a86936
Gabriel Rivas <65317629+Gaborivas22@users.noreply.github.com>
2024-02-06 22:24:25 -0600

    Add files via upload

---
+++ Tarea 7/Arr4x4CalifUPPosit.c
@@ -0,0 +1,44 @@
+/*
+Este programa fue creado por gabriel rivas 
+si lo copias recuerda borrar este cacho o cambiarle el nombre jejej
+*/
+
+#include <stdio.h>
+#include <math.h>
+#define p printf
+#define s scanf
+
+/* 
+Leer calificaciones y almacenarlas en un arreglo de 4x4. Mostrar el arreglo, la calificación más
+alta y en qué posición está almacenada.
+*/
+
+int main() {
+	int i,j,f=0,c=0;
+    float cal[4][4],calMA=0;
+    p("Este programa lee 16 calificaciones y muestra la mas alta junto a su posicion \n");
+    p("Ingrese las calificaciones:\n");
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            s("%f", &cal[i][j]);
+            while(cal[i][j]>10 || cal[i][j]<0){
+            	p("Lo ingresado no es una calificacion\n");
+            	s("%f", &cal[i][j]);
+			}
+        }
+    }
+    p("\nMatriz de calificaciones:\n");
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            p("%.2f\t", cal[i][j]);
+            if (cal[i][j] > calMA) {
+                calMA= cal[i][j];
+                f = i;
+                c = j;
+            }
+        }
+        p("\n");
+    }
+    p("\nLa calificacion más alta es %.2f y se encuentra en la posicion [%d][%d].\n", calMA, f+1, c+1);
+    return 0;
+}
